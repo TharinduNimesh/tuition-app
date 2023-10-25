0c62884e871af5ab10728f3bb88ba27e47df8fe2
Dilini Tharaka <tharindunimesh.abc@gmail.com>
2023-10-25 10:12:58 +0530

    Header Component

---
+++ components/Home/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header id="header">
+    <div class="container-fluid">
+      <div id="logo" class="pull-left">
+        <h1><a href="#intro" class="scrollto">BizPage</a></h1>
+      </div>
+
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <HomeHeaderItem name="Home" path="#intro" isActive="true" />
+          <HomeHeaderItem name="About Us" path="#about" />
+          <HomeHeaderItem name="Services" path="#services" />
+          <HomeHeaderItem name="Portfolio" path="#portfolio" />
+          <HomeHeaderItem name="Team" path="#team" />
+          <HomeHeaderItem name="Contact" path="#contact" />
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+@import url("~/assets/css/home/index.css");
+</style>
---
+++ components/Home/HeaderItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <li :class="{ 'menu-active': isActive }">
+    <a :href="path">{{ name }}</a>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["name", "path", "isActive"],
+};
+</script>
+
+<style>
+@import url("~/assets/css/home/index.css");
+</style>
