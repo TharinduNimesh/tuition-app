91552d081d4ecfd53f1ad0639cbe631ae6ca9997
ChathuraSudaraka <chathurabro68@gmail.com>
2023-11-05 11:16:59 +0530

    About Completed

--- pages/about.vue
+++ pages/about.vue
@@ -3,10 +3,144 @@ useHead({
   title: "About Us | Classmate - Tuition Management System",
 });
 </script>
+
 <template>
   <NuxtLayout name="home">
-      <HomeIntro image="about-us.webp" />
-      <!-- Write Content Here -->
-      About Page
+    <div class="col-lg-12 col-12">
+      <div class="home-intro position-relative">
+        <HomeIntro image="about-us.webp" />
+        <div
+          class="overlay position-absolute top-50 start-0 w-100 h-100 text-center text-white pt-0 pt-md-3"
+        >
+          <h1 class="mb-2 mb-md-4 text-lg fw-bolder">
+            Our mission is to make building websites easier for everyone.
+          </h1>
+          <p class="mb-2 mb-md-4 text-base">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum minima, animi.
+          </p>
+          <a href="#scroll-target" class="btn btn-primary btn-lg"
+            >Read our story</a
+          >
+        </div>
+      </div>
+    </div>
+
+    <section class="py-5 bg-light" id="scroll-target">
+      <div class="container">
+        <header class="section-header" data-aos="fade-up">
+          <div class="row align-items-center">
+            <div class="col-lg-6">
+              <img
+                class="img-fluid rounded mb-5 mb-lg-0"
+                src="https://dummyimage.com/600x400/343a40/6c757d"
+                alt="..."
+              />
+            </div>
+            <div class="col-lg-6">
+              <h3 class="fw">Our founding</h3>
+              <p class="lead fw-normal text-muted mb-0">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+                est, ut esse a labore aliquam beatae expedita. Blanditiis
+                impedit numquam libero molestiae et fugit cupiditate, quibusdam
+                expedita, maiores eaque quisquam.
+              </p>
+            </div>
+          </div>
+        </header>
+      </div>
+    </section>
+
+    <section class="py-5">
+      <div class="container">
+        <header class="section-header" data-aos="fade-up">
+          <div class="row align-items-center">
+            <div class="col-lg-6 order-first order-lg-last">
+              <img
+                class="img-fluid rounded mb-5 mb-lg-0"
+                src="https://dummyimage.com/600x400/343a40/6c757d"
+                alt="..."
+              />
+            </div>
+            <div class="col-lg-6">
+              <h3 class="fw-bolder">Growth &amp; beyond</h3>
+              <p class="lead fw-normal text-muted mb-0">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+                est, ut esse a labore aliquam beatae expedita. Blanditiis
+                impedit numquam libero molestiae et fugit cupiditate, quibusdam
+                expedita, maiores eaque quisquam.
+              </p>
+            </div>
+          </div>
+        </header>
+      </div>
+    </section>
+
+    <section class="py-5 bg-light">
+      <div class="container">
+        <header class="section-header" data-aos="fade-up">
+          <div class="text-center">
+            <h3 class="">Our team</h3>
+            <p class="lead fw-normal text-muted mb-5">
+              Dedicated to quality and your success
+            </p>
+          </div>
+          <div
+            class="row row-cols-1 row-cols-sm-2 row-cols-xl-4 justify-content-center"
+          >
+            <div
+              v-for="(member, index) in teamMembers"
+              :key="index"
+              class="col mb-5"
+            >
+              <div class="text-center">
+                <img
+                  class="img-fluid rounded-circle mb-4 px-4"
+                  :src="member.image"
+                  :alt="member.name"
+                />
+                <h5 class="fw-bolder">{{ member.name }}</h5>
+                <p class="fst-italic text-muted">{{ member.position }}</p>
+              </div>
+            </div>
+          </div>
+        </header>
+      </div>
+    </section>
   </NuxtLayout>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      teamMembers: [
+        {
+          name: "John Doe",
+          position: "Founder & CEO",
+          image: "https://dummyimage.com/150x150/ced4da/6c757d",
+        },
+        {
+          name: "John Doe",
+          position: "CFO",
+          image: "https://dummyimage.com/150x150/ced4da/6c757d",
+        },
+        {
+          name: "John Doe",
+          position: "Operations Manager",
+          image: "https://dummyimage.com/150x150/ced4da/6c757d",
+        },
+        {
+          name: "John Doe",
+          position: "CTO",
+          image: "https://dummyimage.com/150x150/ced4da/6c757d",
+        },
+        {
+          name: "John Doe",
+          position: "CTO",
+          image: "https://dummyimage.com/150x150/ced4da/6c757d",
+        },
+      ],
+    };
+  },
+};
+</script>
