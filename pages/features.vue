5c56e92a6d21ab50d0507ce051cb05adf49db586
ChathuraSudaraka <chathurabro68@gmail.com>
2023-11-08 16:24:00 +0530

    Features Completed

---
+++ pages/features.vue
@@ -0,0 +1,12 @@
+<script setup>
+useHead({
+  title: "Features | Classmate - Tuition Management System",
+});
+</script>
+<template>
+  <NuxtLayout name="home">
+    <HomeIntro image="features.webp" />
+    <!-- Write Content Here -->
+    Features Page
+  </NuxtLayout>
+</template>
